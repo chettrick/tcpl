b2e9a025ed088bb594aa256eb75d73f2afc04f8d
Chris Hettrick <git@structfoo.com>
2014-06-25 21:58:21 -0700

    Implement strrpbrkl(). Clarify the function's comment.

--- ch1/e01-22_foldline.c
+++ ch1/e01-22_foldline.c
@@ -7,8 +7,7 @@
 #define MAXCOL 80	/* Max column width in characters. */
 
 static int getaline(char *, int);
-static const char *strrpbrk(const char *, const char *);
-//static char *strrpbrkl(const char *, const char *, size_t);
+static const char *strrpbrkl(const char *, const char *, size_t);
 
 /*
  * Fold long lines at MAXCOL. Split on whitespace to preserve words.
@@ -20,17 +19,19 @@ main(void)
 	int len;
 	char line[BUFSIZ];	/* Current input line. */
 	const char *s;
-	char *delims = "aeiou";
+	const char *tokens = " \t";
 
 	while ((len = getaline(line, BUFSIZ)) != -1) {
-		s = strrpbrk(line, delims);
+		s = strrpbrkl(line, tokens, MAXCOL);
 		printf("%s", s);
 	}
 
 	return (0);
 }
 
-/* getaline:  read a line into `s'; return length, or -1 if EOF. */
+/*
+ * getaline:  read a line into `s'; return length, or -1 if EOF.
+ */
 static int
 getaline(char *s, int lim)
 {
@@ -52,19 +53,20 @@ getaline(char *s, int lim)
 
 /*
  * The strrpbrkl() function locates in the NUL-terminated string s the
- * last occurrence of any character in the string tok up to len
- * characters and returns a pointer to this character.  If len is NULL,
- * then all of string s is searched.  If no characters in tok occur
- * anywhere in string s within len characters, strrpbrkl() returns NULL.
-	strrpbrkl(const char *s, const char *tok, size_t len)
+ * last occurrence of any character in the NUL-terminated string tok up
+ * to len characters and returns a pointer to this character.  If len is
+ * NUL then all of string s is searched.  If no characters in tok occur
+ * anywhere in s within len characters, strrpbrkl() returns NULL.
  */
 static const char *
-strrpbrk(const char *s, const char *tok)
+strrpbrkl(const char *s, const char *tok, size_t len)
 {
+	size_t i;
 	const char *ptok;
 	const char *ret = NULL;
 
-	for (; *s != '\0'; s++) {
+	for (i = 1; ((i <= len) || (len == '\0')) &&
+	    (*s != '\0'); i++, s++) {
 		for (ptok = tok; *ptok != '\0'; ptok++) {
 			if (*ptok == *s) {
 				ret = s;
