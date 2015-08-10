c9766faa74a867cc3edacdeea1daeb611e81a592
Chris Hettrick <git@structfoo.com>
2015-08-09 20:28:09 -0700

    e01-22_foldline.c: Remove strrpbrkl(). Not used.

--- ch1/e01-22_foldline.c
+++ ch1/e01-22_foldline.c
@@ -4,11 +4,10 @@
 
 #include <stdio.h>
 
-#define MAXCOL 80	/* Max column width in characters. */
+#define MAXCOL	80	/* Max column width in characters. */
 
-static int getaline(char *, int);
-static const char *strrpbrkl(const char *, const char *, size_t);
-static size_t strrcspnl(const char *, const char *, size_t);
+static int	getaline(char *, int);
+static size_t	strrcspnl(const char *, const char *, size_t);
 
 /*
  * Fold long lines at MAXCOL. Split on whitespace to preserve words.
@@ -19,13 +18,11 @@ main(void)
 {
 	int len, span;
 	char line[BUFSIZ];	/* Current input line. */
-	const char *s;
 	const char *tokens = " \t";
 
 	while ((len = getaline(line, BUFSIZ)) != -1) {
 	 	span = strrcspnl(line, tokens, MAXCOL);
 		printf("%d\n", span);
-		s = strrpbrkl(line, tokens, MAXCOL);
 		printf("%s", line);
 	}
 
@@ -52,33 +49,6 @@ getaline(char *s, int lim)
 	s[i++] = '\0';
 
 	return (i);
-}
-
-/*
- * The strrpbrkl() function locates in the NUL-terminated string s the
- * last occurrence of any character in the NUL-terminated string tok up
- * to len characters and returns a pointer to this character.  If len is
- * NUL then all of s is searched.  If no characters in tok occur anywhere
- * in s within len characters, strrpbrkl() returns NULL.
- */
-static const char *
-strrpbrkl(const char *s, const char *tok, size_t len)
-{
-	size_t i;
-	const char *ptok;
-	const char *ret = NULL;
-
-	for (i = 1; ((i <= len) || (len == '\0')) &&
-	    (*s != '\0'); i++, s++) {
-		for (ptok = tok; *ptok != '\0'; ptok++) {
-			if (*ptok == *s) {
-				ret = s;
-				break;
-			}
-		}
-	}
-
-	return (ret);
 }
 
 /*
